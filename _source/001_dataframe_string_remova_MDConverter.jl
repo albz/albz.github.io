a39db9bb9c82d950593bfbdda0d52f77f4906322
Alberto Marocchino <albz.uk@gmail.com>
2018-12-30 11:41:06 +0100

    cleanig up

--- _source/001_dataframe_string_remova_MDConverter.jl
+++
@@ -1,3 +0,0 @@
-using Literate
-cd("C:\\Users\\a.marocchino\\Documents\\codes\\github.io\\_source")
-Literate.markdown("001_dataframe_string_removal.jl", "..\\_posts\\dataframe_string_removal.md"; documenter=false)
